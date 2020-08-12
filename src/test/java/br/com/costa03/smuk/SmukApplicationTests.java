33f84b3e97d774d9d4f630074a9b0b55566f6955
Alexandre Ferreira Costa <alexandrexandefc@gmail.com>
2020-08-12 19:22:17 -0300

    chore: Criando projeto
    Criando projeto Spring Boot

---
+++ src/test/java/br/com/costa03/smuk/SmukApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.costa03.smuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
