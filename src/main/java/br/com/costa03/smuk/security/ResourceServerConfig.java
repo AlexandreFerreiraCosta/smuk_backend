118de128fa16ff6c77290402a47712c21bd87f60
Alexandre Ferreira Costa <alexandrexandefc@gmail.com>
2020-09-08 20:06:29 -0300

    feature : Validando permissão de acesso do usuário
    feature : Validando permissão de acesso do usuário

--- src/main/java/br/com/costa03/smuk/security/ResourceServerConfig.java
+++ src/main/java/br/com/costa03/smuk/security/ResourceServerConfig.java
@@ -1,9 +1,15 @@
 package br.com.costa03.smuk.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -13,10 +19,30 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                        .anyRequest().authenticated()
+                         .antMatchers(HttpMethod.POST , "/api/users/**").hasAnyAuthority("permissionI")
+                .antMatchers(HttpMethod.GET , "/api/users/**").hasAnyAuthority("permission")
+                .anyRequest().denyAll()
                 .and()
                 .cors()
                 .and()
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter(){
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter( jwt -> {
+            var authorities = jwt.getClaimAsStringList("authorities");
+
+            if(authorities == null){
+                authorities = Collections.emptyList();
+            }
+
+            return authorities.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+
+        return jwtAuthenticationConverter;
     }
 }
